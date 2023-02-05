04eb5d4ecb655018e90cd376fafaa52a765b31f0
Paul Praet <3198728+praetp@users.noreply.github.com>
2023-02-05 15:52:47 +0100

    cleanup

--- src/main.sh
+++ src/main.sh
@@ -7,12 +7,16 @@ shopt -s inherit_errexit
 
 INTERVAL=5
 DESTDIR=/opt/homestuff/node_exporter/prometheus-dropzone
+DESTFILE=${DESTDIR}/modbus.prom
+
+rm ${DESTFILE}
+trap "rm ${DESTFILE}" EXIT
 
 TMPFILE=$(mktemp /tmp/modbus_prom.XXXXXXXX)
 [ -d ${DESTDIR} ] || mkdir ${DESTDIR}
 while :
 do
     ./getdata_modbus.sh | tail -n +2 | gawk -f convert.awk > ${TMPFILE}
-    mv ${TMPFILE} ${DESTDIR}/modbus.prom
+    mv ${TMPFILE} ${DESTFILE}
     sleep ${INTERVAL}
 done
